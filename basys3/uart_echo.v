a200c2665247ad30db441c1afb2ddffabfc6c997
Simon D. Levy <simon.d.levy@gmail.com>
2026-08-19 15:54:41 -0400

    Removed CLKS_PER_BIT from Basys3 verilog

--- basys3/uart_echo.v
+++ basys3/uart_echo.v
@@ -1,11 +1,8 @@
 module uart_echo (
-    input  wire clk,      // 12 MHz clock on Cmod A7
+    input  wire clk,      // 100 MHz clock
     input  wire uart_rx,  // USB-UART RX
     output wire uart_tx   // USB-UART TX
 );
-    // 100MHz / 115200 baud = 868 cycles per bit
-    parameter CLKS_PER_BIT = 868; 
-
     reg [7:0] rx_byte = 0;
     wire      rx_dv;
     
